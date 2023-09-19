25799fbddf6a76925794c3ad99db2c1d9243b1de
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-09-19 16:03:35 +1000

    structures the background image, header and search input

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Google Books</h1>
+        <p>Find out about your favourite book!</p>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import styles from "./SearchBar.module.scss";
+
+const SearchBar = () => {
+  return (
+    <form>
+        <input type="text" />
+        <button type="submit">SEARCh</button>
+    </form>
+  )
+}
+
+export default SearchBar
